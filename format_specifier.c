e1d7a91ed159f4f11de5bcbf85315143950f3e21
Gaurav Chaudhary <gauravc@amrrtechsols.com>
2023-04-17 15:49:05 +0530

    format specifier

---
+++ format_specifier.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main()
+{
+    printf("16 = %d decimal Integer\n",16);
+    printf("16 = %.2f floating point number\n",16.3445);
+    printf("16 = %X hexadecimal number\n",16);
+    printf("16 = %u unsigned decimal\n",16);
+}
